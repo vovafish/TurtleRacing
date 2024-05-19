05f878d29fc3121bd2ab1f9115ba7088adf5d5f3
Vladimirs <vladimirribakov123@mail.ru>
2024-05-19 14:31:10 +0100

    Print out all the saved password and account names

--- Projects/Python/Password Manager/main.py
+++ Projects/Python/Password Manager/main.py
@@ -2,7 +2,10 @@ master_pwd = input("What is the master password? ")
 
 
 def view():
-    pass
+    with open("password.txt", "r") as f:
+        for line in f.readlines():
+            print(line.rstrip())
+
 
 
 def add():
@@ -10,7 +13,7 @@ def add():
     pwd = input("Password: ")
 
     with open("password.txt", "a") as f:
-        f.write(name + " | " + pwd)
+        f.write(name + " | " + pwd + "\n")
 
 
 while True:
