f01d7d8ff768bc763ae7fdd18c1c1099c364f385
Vladimirs <vladimirribakov123@mail.ru>
2024-05-21 14:55:31 +0100

    Implemented the encryption and decryption of the password stored in the txt file

--- Projects/Python/Password Manager/main.py
+++ Projects/Python/Password Manager/main.py
@@ -1,11 +1,28 @@
-master_pwd = input("What is the master password? ")
+from cryptography.fernet import Fernet
+
+
+# def write_key():
+#     key = Fernet.generate_key()
+#     with open("key.key", "wb") as key_file:
+#         key_file.write(key)
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+
+key = load_key()
+fer = Fernet(key)
 
 
 def view():
     with open("password.txt", "r") as f:
         for line in f.readlines():
-            print(line.rstrip())
-
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: " + user + "  " + "Password:" + fer.decrypt(passw.encode()).decode())
 
 
 def add():
@@ -13,7 +30,7 @@ def add():
     pwd = input("Password: ")
 
     with open("password.txt", "a") as f:
-        f.write(name + " | " + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
